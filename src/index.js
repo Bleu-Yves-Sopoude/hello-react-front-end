f5af69e278a906bd6bdad2a0ec2dcbb26224c94d
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-31 15:37:08 +0000

    fix linter

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,,
 );
