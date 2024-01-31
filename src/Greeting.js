ebab4638a6bc727c7b789bd9a1d960265dca578f
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-31 15:22:00 +0000

    fix linter

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,23 +1,13 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchMessage } from "./redux/features/greetingsSlice";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMessage } from './redux/features/greetingsSlice';
 
 const Greeting = () => {
   const dispatch = useDispatch();
   const greeting = useSelector((state) => state.greeting.message);
-
   useEffect(() => {
-    if (!greeting) {
-      dispatch(fetchMessage())
-        .then(() => {
-          state.message = action.payload;
-        })
-        .catch((error) => {
-          state.message = "";
-          state.error = action.error.message;
-        });
-    }
-  }, { greeting }[dispatch]);
+    dispatch(fetchMessage());
+  }, [dispatch, greeting]);
 
   return (
     <div>
