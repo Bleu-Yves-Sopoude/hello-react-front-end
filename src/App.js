b103c82ba7ad055c75e8b37d21d1a77cf63f59be
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-31 13:14:24 +0000

    fix linter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Greeting from './Greeting';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Greeting from "./Greeting";
 
 const App = () => (
-
   <Routes>
-    <Route path="/" element={<Greeting />}>
-    </Route>
+    <Route path="/" element={<Greeting />}></Route>
   </Routes>
-
 );
 
 export default App;
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,35 +1,28 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchMessage } from './redux/features/greetingsSlice';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMessage } from "./redux/features/greetingsSlice";
 
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greeting.message);
 
-const Greeting =() =>
-{
+  useEffect(() => {
+    if (!greeting) {
+      dispatch(fetchMessage())
+        .then(() => {
+          console.log("Greeting fetched successfully");
+        })
+        .catch((error) => {
+          console.error("Error fetching greeting:", error);
+        });
+    }
+  }, [dispatch]);
 
-    const dispatch=useDispatch();
-    const greeting= useSelector((state)=> state.greeting.message);
-
-    useEffect(() => {
-        if (!greeting) {
-            dispatch(fetchMessage())
-            .then(() => {
-                console.log("Greeting fetched successfully");
-              })
-              .catch((error) => {
-                console.error("Error fetching greeting:", error);
-              });
-        }
-    }, [dispatch]);
-
-
-    return (
-      <div> 
-            <h1>{greeting}</h1>
-        </div>
-    
-    )
-  
-}
-
+  return (
+    <div>
+      <h1>{greeting}</h1>
+    </div>
+  );
+};
 
 export default Greeting;
