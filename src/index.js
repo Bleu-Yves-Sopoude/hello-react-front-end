0bd8b80faa0cbfb7b8dafa33a05d75ef30d08316
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-31 15:50:48 +0000

    fix linter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-
-      <Route path="/" element={<Greeting />} />
-
-    </Routes>
-  </Router>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 );
+
