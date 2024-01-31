27b6810b06dfae586eceb5bf7c95fa6cb414af03
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-31 15:52:51 +0000

    fix linter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
@@ -14,4 +13,3 @@ root.render(
     </React.StrictMode>
   </Provider>,
 );
-
